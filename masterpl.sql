c17ee78cb7e0b6ca0c9a29462b082b541741971e
mohamed-elsabaa <dev-pl@ukwtsvulx282.eslabs.ssn.hp.com>
2018-09-25 13:52:43 +0100

    ,,

---
+++ masterpl.sql
@@ -0,0 +1,53 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.5.4
+-- Dumped by pg_dump version 9.5.4
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: mpl_main; Type: SCHEMA; Schema: -; Owner: masterpl
+--
+
+CREATE SCHEMA mpl_main;
+
+
+ALTER SCHEMA mpl_main OWNER TO masterpl;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = mpl_main, pg_catalog;
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = mpl_main, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
